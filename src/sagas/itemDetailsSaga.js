576ffb331571a7aacc5820cb49ef84496869d19a
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-28 23:51:06 +0200

    fetching and displaying data using fork and takeLatest

--- src/sagas/itemDetailsSaga.js
+++ src/sagas/itemDetailsSaga.js
@@ -1,5 +1,5 @@
 import { take, fork, put } from 'redux-saga/effects'
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
 
 import {
     SET_CART_ITEMS,
@@ -8,15 +8,17 @@ import {
 
 
 export function* loadItemDetails(item) {
-    console.info("Item?", item)
-    const { id } = item
+    console.info("Item?", item);
+    const { id } = item;
     const response = yield fetch(`http://localhost:8081/items/${id}`)
-    const data = yield response.json()
-    const info = data[0]
-    yield put(setItemDetails(info))
+    const data = yield response.json();
+    const info = data[0];
+    yield put(setItemDetails(info));
 }
 
 export function* itemDetailsSaga() {
-    const { items } = yield take(SET_CART_ITEMS)
-    yield items.map(item=>fork(loadItemDetails, item))
+    const { items } = yield take(SET_CART_ITEMS);
+    console.info(items)
+    yield items.map(item=>fork(loadItemDetails, item));
+
 }
